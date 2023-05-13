62ad2ecbf8cd602f7c8ee868c9c2a3397062215d
IceTheDev2 <cristian.gabriel131408@gmail.com>
2023-05-13 17:05:39 +0300

    random button picked to bear correct answer

--- simple-maths/Assets/CountingManager.cs
+++ simple-maths/Assets/CountingManager.cs
@@ -1,10 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CountingManager : MonoBehaviour
 {
-    public GameObject[] gameObjects;
+    public GameObject[] cubes;
+    public TextMeshProUGUI[] buttonsTexts;
 
     /// <summary>
     /// Called when the script first loads,
@@ -14,15 +17,20 @@ public class CountingManager : MonoBehaviour
     void Start()
     {
         // Generate a random number
-        int randomNumber = Random.Range(0, gameObjects.Length);
+        int randomNumber = Random.Range(0, cubes.Length);
 
         // Disable the first randomNumber objects in the array
-        for (int i = 0; i < gameObjects.Length; i++)
+        for (int i = 0; i < cubes.Length; i++)
         {
             if (i < randomNumber)
             {
-                gameObjects[i].SetActive(false);
+                cubes[i].SetActive(false);
             }
         }
+
+        int activeCubes = 10 - randomNumber;
+
+        int randomButton = Random.Range(0, buttonsTexts.Length);
+        buttonsTexts[randomButton].text = activeCubes.ToString();
     }
 }
